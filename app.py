7135ff5e8383d183863fc25058f9f5f560f29861
seher-alphanumericideas <seher@alphanumericideas.com>
2025-05-29 13:08:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html', title="Home Page")
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About")
+
+if __name__ == '__main__':
+    app.run(debug=True)
